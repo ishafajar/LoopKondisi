8fff517d7776414b54243b5d2ead6672f29c2de2
isa nur fajar <ishafajar11@gmail.com>
2018-01-07 17:16:08 +0700

    Add files via upload

---
+++ LoopingKondisi.java
@@ -0,0 +1,16 @@
+public class LoopingKondisi {
+    public static void main (String[] arg){
+        for (int i = 0; i<=100; i++)
+            if(i%3==0){
+                System.out.println("Isha");
+            }else {
+            if (i%5==0) {
+                    System.out.println("Fajar");
+                }else {
+                System.out.println(i);
+            }
+            }
+
+
+    }
+}
